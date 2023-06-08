6c99d50927acd0adada00996d9ef751bbe3c9bd7
Geundol222 <geundol222@gmail.com>
2023-06-08 20:27:49 +0900

    feat : ScriptableObject

--- Assets/Scripts/TowerPlace.cs
+++ Assets/Scripts/TowerPlace.cs
@@ -22,6 +22,7 @@ public class TowerPlace : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
             BuildInGameUI buildUI = GameManager.UI.ShowInGameUI<BuildInGameUI>("UI/BuildInGameUI");
             buildUI.SetTarget(transform);
             buildUI.SetOffset(new Vector2(0, 200f));
+            buildUI.towerPlace = this;
         } 
     }
 
@@ -33,5 +34,11 @@ public class TowerPlace : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     public void OnPointerExit(PointerEventData eventData)
     {
         render.material.color = normal;
+    }
+
+    public void BuildTower(TowerData data)
+    {
+        GameManager.Resource.Destroy(gameObject);
+        GameManager.Resource.Instantiate(data.Towers[0].tower, transform.position, transform.rotation);
     }
 }
--- Assets/Scripts/UI/InGameUI/BuildInGameUI.cs
+++ Assets/Scripts/UI/InGameUI/BuildInGameUI.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class BuildInGameUI : InGameUI
 {
+    public TowerPlace towerPlace;
+
     protected override void Awake()
     {
         base.Awake();
 
         buttons["Blocker"].onClick.AddListener(() => { GameManager.UI.CloseInGameUI<BuildInGameUI>(this); });
+        buttons["ArcherButton"].onClick.AddListener(BuildArcherTower);
+        buttons["CanonButton"].onClick.AddListener(BuildCanonTower);
+    }
+
+    public void BuildArcherTower()
+    {
+        TowerData archerTowerData = GameManager.Resource.Load<TowerData>("Data/ArcherTowerData");
+        towerPlace.BuildTower(archerTowerData);
+    }
+
+    public void BuildCanonTower()
+    {
+        TowerData canonTowerData = GameManager.Resource.Load<TowerData>("Data/CanonTowerData");
+        towerPlace.BuildTower(canonTowerData);
     }
 }
